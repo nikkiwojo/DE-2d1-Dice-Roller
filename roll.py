b4a9df369c0acb5a8c07ac721356887919637757
Nikki <nikki@zipcodes-MacBook-Pro-3.local>
2021-07-08 20:45:21 -0400

    Final Commit

--- roll.py
+++ roll.py
@@ -1,7 +1,30 @@
-# roll a "die" some number of times.
-# roll - run it once and go into a loop
-# roll 1 - produce a number 1-6 random and print it
-# roll 2 - produce two numbers 1-6 and print them
-# roll 3 - produce three numbers and print them.
-#
+import random
+
+def dice_roll(how_many):
+
+    first = random.randint(1,6)
+    second = random.randint(1,6)
+    third = random.randint(1,6)
+
+    if how_many == 1:
+        print("You rolled a", first)
+    elif how_many == 2:
+        print("First roll:", first, "Second roll:", second)
+    elif how_many == 3:
+        print("First roll:", first, "Second roll:", second, "Third roll:", third)
+
+
+initial_roll = random.randint(1,6)
+
+print("The first roll was a", initial_roll)
+
+roll_again = input("Would you like to roll again? y/n")
+
+while roll_again == "y":
+    number = input("How many dice would you like to roll?")
+    dice_roll(int(number))
+    roll_again = input("Would you like to roll again? y/n")
+
+
+
 
